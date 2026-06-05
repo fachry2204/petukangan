a31ba15635d66228545c5fea97b4690315688863
mvpcyber <mvpcyberjakarta@gmail.com>
2026-06-06 00:28:23 +0700

    Fix Socket.io: run custom server.js in dev mode so Socket.io is active

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { parse } = require('url');
 const next = require('next');
 const { Server } = require('socket.io');
 
-const dev = false;
+const dev = process.env.NODE_ENV !== 'production';
 const port = process.env.PORT || 3000;
 
 const app = next({ dev });
